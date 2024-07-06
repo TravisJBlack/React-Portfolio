cf0df5c3e4c010afc3d254362a5876f2bd5f13de
TravisJBlack <159502505+TravisJBlack@users.noreply.github.com>
2024-07-05 19:51:16 -0500

    inital set up react application. Added router functionality in the main.jsx and create files for pages. also set up import of tailwindcss libary

---
+++ Travis-Portfolio/src/main.jsx
@@ -0,0 +1,48 @@
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './styles/tailwind.css';
+import App from './App'
+//removed css import of vite created css
+// import './index.css'
+
+import ErrorPage from './pages/ErrorPage';
+import HomePage from './pages/HomePage';
+import AboutMe from './pages/AboutMe';
+import Portfolio from './pages/Portfolio';
+import ContactPage from './pages/ContactPage';
+import Resume from './pages/Resume';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: '/AboutMe',
+        element: <AboutMe />,
+      },
+      {
+        path: '/Portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: '/Contact',
+        element: <ContactPage />,
+      },
+      {
+        path: '/Resume',
+        element: <Resume />,
+      },
+    ],
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+)
